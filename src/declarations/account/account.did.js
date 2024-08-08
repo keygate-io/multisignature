f3f5e77bfbbb5970a8b5b51a31f17c0cbe5193ab
Vicente Bermúdez <vicenteb@web3pro.com>
2024-08-08 16:48:55 -0400

    apply .gitignore

--- src/declarations/account/account.did.js
+++
@@ -1,8 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'get_signees' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
-    'hello' : IDL.Func([], [IDL.Text], ['query']),
-    'include_signee' : IDL.Func([IDL.Text], [], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
